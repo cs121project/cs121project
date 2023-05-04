6c5682959a8ea3dff43312e7184f6437b919a766
James <114675435+cs121project@users.noreply.github.com>
2023-05-04 03:08:50 -0700

    Update

--- functions.py
+++ functions.py
@@ -7,10 +7,14 @@ with open('sdgs.json', 'r') as f:
     sdgs = json.load(f)
 
 class SDG:
-    def __init__(self, title, description):
-        self.title
-        self.description
+    def __init__(self, number):
+        self.number = number
     
     def printTitleDes(self):
-        print(f"Title: {sdgs['1']['title']}\nDescription: {sdgs['1']['description']}")
-        print("Make Change")
+        print(f"Title: {sdgs[str(self.number)]['title']}\nDescription: {sdgs['1']['description']}")
+
+NoPoverty = SDG(1)
+
+NoPoverty.printTitleDes()
+
+# print(f"Title: {sdgs['1']['title']}\nDescription: {sdgs['1']['description']}")
