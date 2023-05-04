51da5940a10226226dd756a65faedac21c7f94ed
James <114675435+cs121project@users.noreply.github.com>
2023-05-04 02:24:35 -0700

    Try to update functions.py

---
+++ functions.py
@@ -0,0 +1,12 @@
+import os
+import time
+import json
+import random
+
+class SDG:
+    def __init__(self, title, description):
+        self.title
+        self.description
+    
+    def printTitleDes(self):
+        print()
