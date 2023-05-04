a76cc209ffc715e2bc7a3504873ba8f60108599d
James <114675435+cs121project@users.noreply.github.com>
2023-05-04 02:29:21 -0700

    Add functionalities in function.py

--- functions.py
+++ functions.py
@@ -3,10 +3,13 @@ import time
 import json
 import random
 
+with open('sdgs.json', 'r') as f:
+    sdgs = json.load(f)
+
 class SDG:
     def __init__(self, title, description):
         self.title
         self.description
     
     def printTitleDes(self):
-        print()
+        print(f"Title: {sdgs['1']['title']}\nDescription: {sdgs['1']['description']}")
