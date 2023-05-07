39a32ecc552be1e6812a9224e2231fdbbb815975
zedricedwardc <129153819+zedricedwardc@users.noreply.github.com>
2023-05-07 11:38:22 +0800

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,25 @@
+import random
+import json
+
+def quiz(users,username):
+    print("previous score: ", users[username]["score"])
+    with open('questions.json','r') as f:
+        questions = json.loads(f.read())
+
+    # Shuffle the questions
+    random.shuffle(questions)
+
+    # Quiz loop
+    score = 0   
+    for i in questions:
+        
+        print(f"\n{i['question']} \n {i['options']}\n")
+        user_answer = int(input("Enter the number of your answer: "))
+        if i['options'][int(user_answer)-1] == i['answer']:
+            print("Correct!")
+            score += 1
+        else:
+            print("Incorrect.")
+
+    print(f"final score: {score}") 
+    users[username]["score"]= score
