ebdba646ee57cd0795626b549a2fcc813b091a23
zedricedwardc <129153819+zedricedwardc@users.noreply.github.com>
2023-05-14 16:23:06 +0800

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,25 +1,38 @@
 import random
 import json
 
+def record_score(users,username,score):
+    users[username]["prev_score"]= score
+
+    if score > users[username]["high_score"]:
+        users[username]["high_score"] = score
+
+
+    with open('users.json', 'w') as f:
+        json.dump(users, f , indent=4 )
+
+
 def quiz(users,username):
-    print("previous score: ", users[username]["score"])
+    print("previous score: ", users[username]["prev_score"],"                 highscore: ", users[username]["high_score"])
     with open('questions.json','r') as f:
         questions = json.loads(f.read())
 
     random.shuffle(questions)
 
     score = 0   
-    for i in questions:
-        
-        print(f"\n{i['question']} \n {i['options']}\n")
+    i=questions
+    count = 0
+    while count <= 9:
+        print(f"\n{i[count]['question']} \n [1] {i[count]['options'][0]}\n [2] {i[count]['options'][1]}\n [3] {i[count]['options'][2]}\n [4] {i[count]['options'][3]}\n")
+
         user_answer = int(input("Enter the number of your answer: "))
-        if i['options'][int(user_answer)-1] == i['answer']:
+        if i[count]['options'][int(user_answer)-1] == i[count]['answer']:
             print("Correct!")
             score += 1
         else:
             print("Incorrect.")
+        count += 1
+
 
     print(f"final score: {score}") 
-    users[username]["score"]= score
-    with open('users.json', 'w') as f:
-        json.dump(users, f , indent=4 )
+    record_score(users,username,score)
