0c1a6ca35e41a7a4609a8c06827901228e7c3665
James <114675435+cs121project@users.noreply.github.com>
2023-05-21 02:36:52 -0700

    Update, removed functions.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import time
-from functions import *
+import json
+import random
 
 os.system('cls')
 
@@ -83,4 +84,44 @@ def SDGs():
         os.system('cls')
         SDGs()
 
-start()
+with open('sdgs.json', 'r') as f:
+    sdgs = json.load(f)
+
+class SDG:
+    def __init__(self, number):
+        self.number = number
+    
+    def printTitleDes(self):
+        os.system('cls')
+        print(f"Title: {sdgs[str(self.number)]['title']}\nDescription: {sdgs['1']['description']}\n")
+            
+        while True:
+            FunctionsYesOrNo = input("Would you like to return to main menu or exit? [Menu/Exit]: ").lower()
+            if FunctionsYesOrNo == "menu":
+                os.system('cls')
+                start()
+            elif FunctionsYesOrNo == "exit":
+                exit()
+            else:
+                print("\nPlease choose only from the option.\n")
+                continue
+
+NoPoverty = SDG(1)
+ZeroHunger = SDG(2)
+GoodHealthAndWellBeing = SDG(3)
+QualityEducation = SDG(4)
+GenderEquality = SDG(5)
+CleanWaterAndSanitation = SDG(6)
+AffordableAndCleanEnergy = SDG(7)
+DecentWorkAndEconomicGrowth= SDG(8)
+IndustryInnovationAndInfrastructure= SDG(9)
+ReducedInequalities = SDG(10)
+SustainableCitiesAndCommunities = SDG(11)
+ResponsibleConsumptionAndProduction = SDG(12)
+ClimateAction = SDG(13)
+LifeBelowWater = SDG(14)
+LifeOnLand= SDG(15)
+PeaceJusticeAndStrongInstitutions = SDG(16)
+PartnershipsForTheGoals = SDG(17)
+
+start() # Where everything starts
